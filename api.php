904b5f94e3ffc7c3f2ffdba38a612e55f7ed1df4
titankou2002 <titankou2002@gmail.com>
2026-06-10 21:02:00 +0800

    add error catching, fix header conflicts

--- api.php
+++ api.php
@@ -1,4 +1,20 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+header('Content-Type: application/json; charset=utf-8');
+
+// 捕獲所有錯誤
+set_error_handler(function ($severity, $msg, $file, $line) {
+    throw new ErrorException($msg, 0, $severity, $file, $line);
+});
+register_shutdown_function(function () {
+    $e = error_get_last();
+    if ($e && in_array($e['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode(['success' => false, 'msg' => '重大錯誤: ' . $e['message'], 'file' => $e['file'], 'line' => $e['line']]);
+    }
+});
+
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/src/GoogleSheetsClient.php';
 require_once __DIR__ . '/src/SleeperService.php';
--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
-// config.php - 睡美人戰情室設定
 define('SS_ID_MAIN', getenv('SS_ID_MAIN') ?: '1G5q-GixMWSdJJeF8ZiXWMOfrx4FMobER25jNc8m4Zds');
 
 define('SLEEPER_SHEET',   '睡美人');
@@ -9,10 +8,3 @@ define('PRICE_SHEET',     '編號價目表');
 define('STOCK_SHEET',     '庫存表');
 
 define('SERVICE_ACCOUNT_FILE', __DIR__ . '/service-account.json');
-
-// CORS headers
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(204); exit; }
-header('Content-Type: application/json; charset=utf-8');
