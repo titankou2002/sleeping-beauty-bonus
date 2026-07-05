30fe19ce616511acf4493183f653b24faeca76bb
titankou2002 <titankou2002@gmail.com>
2026-07-06 06:53:04 +0800

    fix: clear-cache token 內建，不需 env var

--- clear-cache.php
+++ clear-cache.php
@@ -1,5 +1,5 @@
 <?php
-if (($_GET['token'] ?? '') !== getenv('CACHE_CLEAR_TOKEN')) {
+if (($_GET['token'] ?? '') !== 'be4109677a907a4e5fab34f9bf302fb2') {
     http_response_code(403);
     exit('Forbidden');
 }
