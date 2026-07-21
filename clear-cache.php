1d023ae036f652172960b6681776da4b867d87a9
titankou2002 <titankou2002@gmail.com>
2026-07-21 10:27:35 +0800

    fix: clear-cache v2 強制更新 checksum

--- clear-cache.php
+++ clear-cache.php
@@ -1,11 +1,12 @@
 <?php
+// v2
 if (($_GET['token'] ?? '') !== 'be4109677a907a4e5fab34f9bf302fb2') {
     http_response_code(403);
     exit('Forbidden');
 }
 if (function_exists('opcache_reset')) {
     opcache_reset();
-    echo 'OPcache cleared.';
+    echo 'OPcache cleared. ' . date('Y-m-d H:i:s');
 } else {
-    echo 'OPcache not available.';
+    echo 'OPcache not available. ' . date('Y-m-d H:i:s');
 }
